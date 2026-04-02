44740a59ad158bc39dd485749ab166b390ab8f9f
yuan199696 <yuan199696@qq.com>
2026-04-02 17:59:25 +0800

    fix

--- src/stubs/ant-packages/color-diff-napi/index.js
+++ src/stubs/ant-packages/color-diff-napi/index.js
@@ -1,4 +1,9 @@
 // Stub for color-diff-napi (native addon, not published)
-export class ColorDiff { diff() { return [] } }
-export class ColorFile {}
+export class ColorDiff {
+  diff() { return [] }
+  render() { return null }
+}
+export class ColorFile {
+  render() { return null }
+}
 export function getSyntaxTheme() { return {} }
